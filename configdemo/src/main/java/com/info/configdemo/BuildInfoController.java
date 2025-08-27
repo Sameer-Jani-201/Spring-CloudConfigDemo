da6ca10cd9dba3e96985539ccbddd538573f8378
Sameer Jani <sameerjani.it@gmail.com>
2025-08-28 00:12:15 +0530

    Update config and make remote config privately access

--- configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -1,11 +1,13 @@
 package com.info.configdemo;
 
 import lombok.AllArgsConstructor;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RefreshScope
 public class BuildInfoController {
 
     private BuildInfo buildInfo;
